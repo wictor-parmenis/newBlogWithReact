5a2bb453ed3ed324223d5d8e61930cc4b6cebee9
wictor-parmenis <wictortec@gmail.com>
2021-02-27 22:00:38 -0300

    Reinstall react-router-dom

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react';
+import './dashboard.css'
+import {Link, withRouter} from 'react-router-dom'
 
-export default class Dashboard extends Component{
+class Dashboard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name: 'Wictor'
+        };
+
+        // Binded functions
+    }
+
+    // Methods here
+
+
+
     render(){
         return(
             <div>
-                <h2>Dashboard here</h2>
+                <h2>Hello {this.state.name}</h2>
             </div>
         )
     }
 }
+
+export default withRouter(Dashboard);
